25c17d73275b7fdba69c5b0a5dadb632713b793c
Fahmudin Yusuf <122071540+Fahmudin@users.noreply.github.com>
2023-02-14 12:13:40 -0600

    Add files via upload

---
+++ meow.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void)
+
+{
+    int x = 0;
+    while (x < 4)
+    {
+      printf("Meow!\n");
+      x++;
+    }
+
+    int i = 0;
+    while (i < 5)
+    {
+        printf("Woof!\n");
+        i++;
+    }
+
+    int j = 0;
+    while (j < 6)
+    {
+      printf("mooooooooo!");
+    }
+}
